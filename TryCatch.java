375626703d434b1d311383b127c23227e88de55a
Najmalfawaz <76723512+Najmalfawaz@users.noreply.github.com>
2021-09-14 13:17:50 +0530

    Add files via upload

---
+++ TryCatch.java
@@ -0,0 +1,17 @@
+public class TryCatch
+{
+	public static void main(String args[])
+	{
+	int a=10; int b=5,c=5; int x,y;
+	try
+	{
+	x=a/(b-c);
+	}
+	catch(ArithmeticException e)
+	{
+	System.out.println("Divides by zero");
+	}
+	y=a/(b+c);
+	System.out.println("y= " +y);
+	}
+}
